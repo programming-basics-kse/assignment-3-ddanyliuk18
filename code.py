fbbf30d551b07c94d0d194b0624c033b3ad039f6
ddanyliuk18 <ddanyliuk@kse.org.ua>
2024-11-20 20:20:22 +0200

    modifyed

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import argparse
+
 parser = argparse.ArgumentParser("Olympic Athletes")
 parser.add_argument("file", help="filepath")
 parser.add_argument("-medals", help="sorted medals")
@@ -6,3 +7,9 @@ parser.add_argument("country", help="code of the country")
 parser.add_argument("year", help="year of olympic")
 parser.add_argument("-output", help="filepath to save")
 args = parser.parse_args()
+
+with open('athlete_events.csv', newline='', encoding='utf-8') as file:
+    lines = file.readlines()
+    head = lines[0].strip().split(",")
+    for line in lines[1:]:
+        data = line.strip().split(",")
