68b2ca1287cb30af13431a0b6c40ce18811aef7a
lchervonetska <lchervonetska@kse.org.ua>
2024-11-21 11:42:45 +0200

    dict have created of all datas

--- code.py
+++ code.py
@@ -1,4 +1,18 @@
 import argparse
+
+def read_file():
+    with open('athlete_events.csv', "r", encoding='utf-8') as file:
+        lines = file.readlines()
+        head = lines[0].strip().split(",")
+        all_data = []
+        for line in lines[1:]:
+            data = line.strip().split(",")
+            rows = {}
+            for i in range(len(head)):
+                rows[head[i]] = data[i]
+            all_data.append(rows)
+    return all_data
+
 parser = argparse.ArgumentParser("Olympic Athletes")
 parser.add_argument("file", help="filepath")
 parser.add_argument("-medals", help="sorted medals")
@@ -6,4 +20,6 @@ parser.add_argument("country", help="code of the country")
 parser.add_argument("year", help="year of olympic")
 parser.add_argument("-output", help="filepath to save")
 args = parser.parse_args()
+
+print(read_file())
 
